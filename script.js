591dd5bdc2a28e1f79dda373a1703daa7b77dc65
nasimbillah <nasimbillah269@gmail.com>
2022-09-14 22:12:43 +0600

    added time counter

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
@@ -124,7 +124,12 @@ const start = () => {
 };
 
 // START Countdown
-startBtn.addEventListener("click", start);
+document.getElementById('starts').addEventListener('click', function () {
+  // console.log('btn click');
+  start()
+
+})
+// startBtn.addEventListener("click", start);
 
 // If history exists, show it
 displayHistory();
